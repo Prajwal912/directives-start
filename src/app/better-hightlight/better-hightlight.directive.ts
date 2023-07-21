bd97429c8a1a4bb3dede99dc1c7f5062a5707d1b
prajwal <prajwalshah912@gmail.com>
2023-07-21 23:33:58 +0530

    used hostlistener to access the dom by using the event using hostlistener

--- src/app/better-hightlight/better-hightlight.directive.ts
+++ src/app/better-hightlight/better-hightlight.directive.ts
@@ -1,4 +1,4 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef, HostListener } from '@angular/core';
 import { Directive, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
@@ -9,13 +9,23 @@ export class BetterHightlightDirective implements OnInit {
   constructor(private elRef:ElementRef, private renderer: Renderer2) { }
 
 ngOnInit(): void {
-  this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue' )
+  this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey' )
+  // this.renderer.setStyle(this.elRef.nativeElement, 'color','white' )
+}
+
+@HostListener('mouseenter') mouseover(evenetData: Event){
+  this.renderer.setStyle(this.elRef.nativeElement, 'color','red' )
+}
+@HostListener('mouseleave') mouseleave(evenetData: Event){
   this.renderer.setStyle(this.elRef.nativeElement, 'color','white' )
 }
 
 }
+//by using hostlistener we can handle the dom events useing the renderer to access the dom elements
 
+// In Angular, Renderer2 is a service provided by the Angular framework to manipulate the DOM safely.
+// It is used to interact with the underlying DOM elements without directly accessing or modifying them through ElementRef or using vanilla JavaScript DOM manipulation.
+// The use of Renderer2 ensures that your application remains secure and is compatible with server-side rendering or web workers.
 
-// In Angular, Renderer2 is a service provided by the Angular framework to manipulate the DOM safely. It is used to interact with the underlying DOM elements without directly accessing or modifying them through ElementRef or using vanilla JavaScript DOM manipulation. The use of Renderer2 ensures that your application remains secure and is compatible with server-side rendering or web workers.
-
-// The main reason for using Renderer2 instead of directly manipulating the DOM is that it allows Angular to work seamlessly with different rendering engines, such as those used in browsers or server-side rendering.
+// The main reason for using Renderer2 instead of directly manipulating the DOM is that it allows Angular to
+// work seamlessly with different rendering engines, such as those used in browsers or server-side rendering.
